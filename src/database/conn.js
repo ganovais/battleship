65ad29fe8f82575fd732c02957b2dc5926bfafd6
ganovais <gabriel_novais1997@hotmail.com>
2022-10-18 01:03:45 -0300

    mongodb config

--- src/database/conn.js
+++ src/database/conn.js
@@ -1,12 +1,21 @@
-import { MongoClient } from "mongodb";
+import { MongoClient, ServerApiVersion } from "mongodb";
 import "dotenv/config";
 
 // const connectionString = `mongodb://${process.env.HOST || "mongodb"}:27017`;
-const connectionString = 'mongodb+srv://ganovais:ganovaismongodb@cluster0.ope2i1f.mongodb.net/?retryWrites=true&w=majority'
-const client = new MongoClient(connectionString, {
+// const connectionString = 'mongodb+srv://ganovais:ganovaismongodb@cluster0.ope2i1f.mongodb.net/?retryWrites=true&w=majority'
+// const client = new MongoClient(connectionString, {
+//    useNewUrlParser: true,
+//    useUnifiedTopology: true,
+// });
+
+const uri =
+   "mongodb+srv://ganovais:ganovaismongodb@cluster0.ope2i1f.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
    useNewUrlParser: true,
    useUnifiedTopology: true,
+   serverApi: ServerApiVersion.v1,
 });
+
 
 let dbConnection = null;
 
